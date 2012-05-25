820c73ab1f0801371f33b3827ed661bca2f82317
Jochen Skulj <jochen@jochenskulj.de>
2012-05-25 13:42:15 +0200

    Setup updated.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
-setup(name = "CryptBox",
+setup(name = "cryptbox",
     version = "0.1",
     description = "Tool for synchronizing files supporting encryption",
     author = "Jochen Skulj",
     author_email = "jochen@jochenskulj.de",
     url = "https://github.com/joskulj/cryptbox/",
-    packages = ['cryptbox'],
+    packages = ["cryptbox"],
+    package_data = { "cryptbox" : ["*.glade"] },
     scripts = ["cryptbox-runner"],
     long_description = """Really long text here.""" 
 ) 
