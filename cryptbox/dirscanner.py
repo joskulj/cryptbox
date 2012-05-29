6763fe6cc738579d61b2a329907a33ae5fb784d5
Jochen Skulj <jochen@jochenskulj.de>
2012-05-29 18:01:53 +0200

    Temporary filenames will be ignored.

--- cryptbox/dirscanner.py
+++ cryptbox/dirscanner.py
@@ -15,8 +15,12 @@
 import glob
 import os
 import os.path
+import re
 
 from fileinfo import *
+
+# Regular expressions for temporary lock files
+TEMP_PATTERNS = ["'\..+\.swp", ".+\.lock", "\.~lock\..+#"]
 
 class DirScanner(object):
     """
@@ -30,9 +34,43 @@ class DirScanner(object):
         - rootpath
           path of the root to scen
         """
+        self._pattern_list = self._init_patterns(TEMP_PATTERNS)
         self._list = FileInfoList()
         self._rootpath = rootpath
         self._scan(self._rootpath)
+
+    def _init_patterns(self, pattern_list):
+        """
+        initializes the list of patterns for identifying
+        temporary lock files
+        Parameters:
+        - pattern list
+          list of regular expressions
+        Returns:
+        - list of pattern instances
+        """
+        result = []
+        for pattern_string in pattern_list:
+            p = re.compile(pattern_string)
+            result.append(p)
+        return result
+
+    def _is_lockfile(self, filename):
+        """
+        checks if a filename is a temporary filename
+        Parameters:
+        - filename
+          filename to check
+        Returns:
+        - True:  file is a temporary file
+        - False: file is not a temporary file
+        """
+        result = False
+        for p in self._pattern_list:
+            m = p.match(filename)
+            if m != None:
+                result = True
+        return result
 
     def _scan(self, path):
         """
@@ -45,9 +83,11 @@ class DirScanner(object):
         dirlist = []
         for entry in glob.glob(os.path.join(path, "*")):
             if os.path.isfile(entry):
-                fileinfo = FileInfo(self._rootpath, entry)
-                fileinfo.scan()
-                self._list.append(fileinfo)
+                basename = os.path.basename(entry)
+                if not self._is_lockfile(basename):
+                    fileinfo = FileInfo(self._rootpath, entry)
+                    fileinfo.scan()
+                    self._list.append(fileinfo)
             if os.path.isdir(entry):
                 dirlist.append(entry)
         for entry in dirlist:
